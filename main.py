e843be8c6f44338f17ed96592ec03baa536572b3
andrewsena1994 <andrewsena1994@gmail.com>
2025-10-24 05:31:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Preciosa IA – API MVP")
 # CORS p/ localhost do front
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "https://preciosa-ia-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
